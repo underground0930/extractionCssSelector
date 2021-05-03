4069910ae8f04352a2670dc698f248f585505ce1
underground0930 <go.nishiduka.1985@gmail.com>
2021-05-03 15:55:20 +0900

    space対応

--- src/extension.ts
+++ src/extension.ts
@@ -6,36 +6,38 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "extension.extractionCssSelector",
     async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const { document, selection } = editor;
-        const text = document.getText(
-          selection.isEmpty ? undefined : selection
-        );
-        const classes = searchSelector(text);
-        if (classes.length > 0) {
-          let value = undefined;
-          const isInput = await vscode.window.showInformationMessage(
-            "セレクタ内に文字列を入力しますか？",
-            "Yes",
-            "No"
-          );
-          if (isInput === "Yes") {
-            value = await vscode.window.showInputBox({
-              prompt: "セレクタ内に入力したい文字列を入力",
-            });
-          }
-
-          await vscode.env.clipboard.writeText(setCssSelectors(classes, value));
-
-          vscode.window.showInformationMessage(
-            "cssセレクタをクリップボードにコピーしました"
-          );
-
-          return;
-        }
-        vscode.window.showInformationMessage("classが見つかりませんでした");
+      const {
+        activeTextEditor,
+        showInformationMessage,
+        showInputBox,
+      } = vscode.window;
+      if (!activeTextEditor) {
+        // アクティブなエディタがない場合
+        return;
       }
+      const { document, selection } = activeTextEditor;
+      const text = document.getText(selection.isEmpty ? undefined : selection);
+      const classes = searchSelector(text);
+      if (classes.length === 0) {
+        // classが見つからない場合
+        showInformationMessage("classが見つかりませんでした");
+        return;
+      }
+      const { clipboard } = vscode.env;
+      let value = undefined;
+      const isInput = await showInformationMessage(
+        "セレクタ内に文字列を入力しますか？",
+        "Yes",
+        "No"
+      );
+      if (isInput === "Yes") {
+        value = await showInputBox({
+          prompt: "セレクタ内に入力したい文字列を入力",
+        });
+      }
+      await clipboard.writeText(setCssSelectors(classes, value));
+      showInformationMessage("cssセレクタをクリップボードにコピーしました");
+      return;
     }
   );
 
--- src/searchSelector.ts
+++ src/searchSelector.ts
@@ -2,8 +2,12 @@ const searchSelector = (text: string): string[] => {
   const regex = /(class|className)=(["'])(.+?)\2/g;
   let matches;
   let classes = [];
+  const spaceRegex = /\s{2,}/;
   while ((matches = regex.exec(text)) !== null) {
-    classes.push(matches[3].split(" "));
+    let result = matches[3].trim(); // class部分の文字列を抽出 & 端のスペースを除去
+    result = result.replace(/　/g, " "); // 全角スペースを半角スペースにする
+    result = result.replace(spaceRegex, " "); // 連続するスペースを１つに変換
+    classes.push(result.split(" "));
   }
   classes = classes.flat();
   classes = Array.from(new Set(classes));
